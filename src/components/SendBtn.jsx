ba723709f12a169c640a007d77b7b49299033a9f
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-12-27 19:12:01 +0530

    Add message send

--- src/components/SendBtn.jsx
+++ src/components/SendBtn.jsx
@@ -1,9 +1,25 @@
 import '../assets/styles/sendBtn.css'
+import send from '../assets/images/send.png'
+import star from '../assets/images/star.png'
 
 const SendBtn = () => {
     return (
         <div className="send">
-            
+            <div className="send-msg">
+                <input type="text" />
+                <div className='send-btn'>
+                    <img src={send} alt="send icon" />
+                </div>
+            </div>
+            <div className="suggestions">
+                <img src={star} alt="star icon" />
+                <div className="sug">
+                    Show me more related items
+                </div>
+                <div className="sug">
+                    Show me related logs from nexus-front-end
+                </div>
+            </div>
         </div>
     )
 }
